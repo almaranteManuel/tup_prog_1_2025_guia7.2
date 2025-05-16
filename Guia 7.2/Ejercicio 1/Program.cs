863e635ecf76a151d0a01a804e299be8c460c0c1
= <=>
2025-05-16 20:09:51 -0300

    Subiend ejercicio 1, 2 y probando gitignore

---
+++ Guia 7.2/Ejercicio 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la nota necesaria para la promoción");
+            double notaPromo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la nota del primer examen");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del segundo examen");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese la nota del tercer examen");
+            double nota3 = Convert.ToDouble(Console.ReadLine());
+
+            double promedio = (nota1 + nota2 + nota3) / 3;
+
+            if (promedio >= notaPromo)
+            {
+                Console.WriteLine("El alumno está promovido");
+            }
+            else
+            {
+                Console.WriteLine("El alumno no está promovido, rinde final");
+            }
+        }
+    }
+}
---
+++ Guia 7.2/Ejercicio 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número");
+
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("El número es negativo");
+            }
+            else if (numero == 0)
+            {
+                Console.WriteLine("El número es cero");
+            }
+            else
+            {
+                Console.WriteLine("El número es positivo");
+            }
+        }
+    }
+}
